1b4f41c35357b9f3764eab9f6f594d4155d80738
DmytroZnoskov <68749902+DmytroZnoskov@users.noreply.github.com>
2023-11-26 00:25:48 +0200

    Add files via upload

---
+++ DmytroZnoskovLab4/2)/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const int maxSize = 100; // Максимальний розмір масиву
+    int n;
+
+    // Введення розміру масиву
+    std::cout << "Введіть розмір масиву (n): ";
+    std::cin >> n;
+
+    // Перевірка на коректність розміру масиву
+    if (n <= 0 || n > maxSize) {
+        std::cout << "Некоректний розмір масиву. Будь ласка, введіть розмір від 1 до " << maxSize << ".\n";
+        return 1; // Помилка виконання програми
+    }
+
+    // Оголошення та ініціалізація масиву
+    int arr[maxSize];
+
+    // Введення елементів масиву
+    for (int i = 0; i < n; ++i) {
+        std::cout << "Введіть елемент масиву [" << i << "]: ";
+        std::cin >> arr[i];
+    }
+
+    // Знайдемо останній негативний елемент та обчислимо суму цілих частин елементів після нього
+    int lastNegativeIndex = -1;
+    for (int i = n - 1; i >= 0; --i) {
+        if (arr[i] < 0) {
+            lastNegativeIndex = i;
+            break;
+        }
+    }
+
+    if (lastNegativeIndex == -1) {
+        std::cout << "У масиві немає негативних елементів.\n";
+    }
+    else {
+        int sum = 0;
+        for (int i = lastNegativeIndex + 1; i < n; ++i) {
+            sum += static_cast<int>(arr[i]);
+        }
+
+        std::cout << "Сума цілих частин елементів, розташованих після останнього негативного: " << sum << std::endl;
+    }
+
+    return 0;
+}
---
+++ DmytroZnoskovLab4/3)/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <algorithm>
+#include<Windows.h>
+
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+        
+    const int maxSize = 100; // Максимальний розмір масиву
+    int n;
+
+    // Введення розміру масиву
+    std::cout << "Введіть розмір масиву (n): ";
+    std::cin >> n;
+
+    // Перевірка на коректність розміру масиву
+    if (n <= 0 || n > maxSize) {
+        std::cout << "Некоректний розмір масиву. Будь ласка, введіть розмір від 1 до " << maxSize << ".\n";
+        return 1; // Помилка виконання програми
+    }
+
+    // Оголошення та ініціалізація масиву
+    double arr[maxSize];
+
+    // Введення елементів масиву
+    for (int i = 0; i < n; ++i) {
+        std::cout << "Введіть речовий елемент масиву [" << i << "]: ";
+        std::cin >> arr[i];
+    }
+
+    // Знаходимо максимальний елемент
+    double maxElement = *std::max_element(arr, arr + n);
+
+    // Сортуємо масив так, щоб спочатку розташовувалися всі елементи, що відрізняються від максимального не більше ніж на 20%
+    std::sort(arr, arr + n, [maxElement](double a, double b) {
+        return std::abs(a - maxElement) <= 0.2 * maxElement;
+        });
+
+    // Виведення результату
+    std::cout << "Перетворений масив:\n";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
